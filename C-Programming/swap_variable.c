56d49f6da7095ba5a54cc94b37bbe9d311cf6517
N G ARUN <92279619+ARUNNG2004@users.noreply.github.com>
2022-11-27 07:52:51 -0800

    Update swap_variable.c

--- C-Programming/swap_variable.c
+++ C-Programming/swap_variable.c
@@ -13,8 +13,8 @@ int main()
 	int a,b;
 	printf("\nEnter The Value Of A & B:");
 	scanf("%d%d",&a,&b);
-	printf("\Before swap A : %d | B : %d",a,b);
+	printf("\nBefore swap A : %d | B : %d",a,b);
 	swap(&a,&b);
-	printf("\After swap A : %d | B : %d",a,b);
+	printf("\nAfter swap A : %d | B : %d",a,b);
 	return 0;
 }
