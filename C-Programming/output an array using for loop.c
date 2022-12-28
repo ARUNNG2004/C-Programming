3107fb162c0b9c65f791c58a9da59b7178a81c3b
agilarasu <86509617+agilarasu@users.noreply.github.com>
2022-12-28 21:27:44 +0530

    Update and rename input and outpu.c to output an array using for loop.c

--- C-Programming/output an array using for loop.c
+++ C-Programming/output an array using for loop.c
@@ -3,10 +3,14 @@ void main()
 {
 int marks[5]={10,20,30,40,50};
 int i=0,counter=0;
-printf("Enter a number less than 5");
+printf("Enter a number less than 5\n");
 scanf("%d",&counter);
     for(i=0 ;i<counter ; i++)
     {
+        if counter > 4
+        {
+        break
+            }
         printf("%d\n",marks[i]);
     }
 }
